345aba391db34c20048be80c8479780c41821a94
Maciej Firek <Maciej.Firek@microsoft.wsei.edu.pl>
2022-05-25 19:12:58 +0200

    2

--- Salon/SalonClass/salonClass.cs
+++ Salon/SalonClass/salonClass.cs
@@ -13,5 +13,35 @@ namespace Salon.SalonClass
         public string Pesel { get; set; }
         public string email { get; set; }
         public string telefon { get; set; }
+
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        public DataTable Select()
+        {
+
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+
+                string sql = "SELECT * FROM Klienci";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+
+        }
     }
 }
