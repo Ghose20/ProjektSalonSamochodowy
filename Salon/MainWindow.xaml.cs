f1493e1dc52f23fc2463843b70ba362fa7b14b82
Maciej Firek <Maciej.Firek@microsoft.wsei.edu.pl>
2022-05-25 19:02:32 +0200

    Add project files.

---
+++ Salon/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using Salon.SalonClass;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace Salon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Salon/SalonClass/salonClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Salon.SalonClass
+{
+    internal class salonClass
+    {
+    }
+}
