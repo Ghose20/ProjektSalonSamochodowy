e8c1a4773568d2bb1be5a4c1fd7b26171bb22f18
Maciej Firek <Maciej.Firek@microsoft.wsei.edu.pl>
2022-05-25 19:10:53 +0200

    1

--- Salon/SalonClass/salonClass.cs
+++ Salon/SalonClass/salonClass.cs
@@ -7,5 +7,11 @@ namespace Salon.SalonClass
 {
     internal class salonClass
     {
+        public int ID { get; set; }
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public string Pesel { get; set; }
+        public string email { get; set; }
+        public string telefon { get; set; }
     }
 }
